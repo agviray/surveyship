dbf87756feb000a10348f741733179917b909c22
Allan Viray <allang.v.dev@gmail.com>
2023-08-21 19:27:57 -0500

    Update index.js to test Heroku subsequent deploy success

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // - Creates route handler and associates it with the given route, '/'.
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hello: 'Test' });
 });
 
 const PORT = process.env.PORT || 5000;
