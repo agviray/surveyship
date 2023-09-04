a10de9777c620b52a19020fdff4f0164480d193d
Allan Viray <allang.v.dev@gmail.com>
2023-09-04 16:23:00 -0500

    Implement serializeUser function to generate user.id

--- services/passport.js
+++ services/passport.js
@@ -8,6 +8,12 @@ const keys = require('../config/keys');
 // - Create a reference to the model class that's used for a user.
 const User = mongoose.model('users');
 
+// - Takes in a user model and generates an identifying piece of info (user.id) to be
+//   passed back to client in form of a cookie.
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -18,7 +24,12 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       // - Here, the user returns from the Google OAuth flow with their
       //   Google ID in hand.
-      // - Now we check to see if their Google ID exists in the database.
+      // - The idea here is to return a user record--but before doing so,
+      //   a check needs to occur to see if the user has an existing record in
+      //   the Mongo database, or if they are new (which if new, a new record must be created
+      //   and added to the Mongo database).
+      // - Ultimately, a user record will be returned whether it's a new user record or
+      //   an existing one.
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
           // - We already have a record with the given profile ID, so
@@ -26,7 +37,9 @@ passport.use(
           done(null, existingUser);
         } else {
           //  - We don't have a user record with this ID, so make
-          //    a new record and save it in the database!
+          //    a new record and save it in the database.
+          // - Now with this new user record created and saved, we can proceed to do
+          //   something with it.
           new User({
             googleId: profile.id,
           })
