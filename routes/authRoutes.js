567a5e58c2d4765b4d5876ab020decd2a2b5537f
Allan Viray <allang.v.dev@gmail.com>
2023-09-11 21:06:57 -0500

    Add route handler to log out user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,6 +14,12 @@ module.exports = (app) => {
   // - Route handler to handle case where user is sent to/visits '/auth/google/callback'
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  // - Log user out of application when they visit provided string route.
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   // - Route handler to handle case where user visits string route shown below.
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
