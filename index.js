a8a37fe60afbc829072c83fdb2476d1c8c1cf965
Allan Viray <allang.v.dev@gmail.com>
2023-09-11 20:00:37 -0500

    Install cookie-session to enable with cookie usage
    - Tell app to enable cookie usage, and tell PassportJS to make use of cookies.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +11,19 @@ mongoose.connect(keys.mongoURI);
 
 // - Creates Express application
 const app = express();
+
+// - Enable cookie usage with Express.
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+// - Tell passport to make use of cookies in order to keep track of
+//   our "user authentication state".
+app.use(passport.initialize());
+app.use(passport.session());
 
 // - Ensure route handlers are useable by making
 //   app accessible to the route handlers.
