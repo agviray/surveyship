8985418f8c8320344b51f7ae77a23f0e10ae2380
Allan Viray <allang.v.dev@gmail.com>
2023-09-26 20:22:25 -0500

    Create initial Header component and implement

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// Using class based component.
+class Header extends Component {
+  render() {
+    return <div>Header</div>;
+  }
+}
+
+export default Header;
