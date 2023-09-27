1825b2b57b30fd4bcefd16e3908871fd997956db
Allan Viray <allang.v.dev@gmail.com>
2023-09-26 20:15:16 -0500

    Set up initial react-router-dom setup in App.js
    - Use temporary components as Routes.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
     <div>
-      <h1>Hello SurveyShip!</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact={true} path="/" element={<Landing />} />
+          <Route path="/surveys" element={<Dashboard />} />
+          <Route path="/surveys/new" element={<SurveyNew />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
