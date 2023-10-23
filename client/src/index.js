7e719b0a6b27f56b524650f950ebae894e58a5bd
Allan Viray <allang.v.dev@gmail.com>
2023-10-23 12:11:46 -0500

    Update surveyTemplate content

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,11 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// - Temporary code for testing email sending
+// - For development only. Delete later.
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
