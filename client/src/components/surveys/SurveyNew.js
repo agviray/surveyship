410180271f2569da54bc7308b321bf905b006856
Allan Viray <allang.v.dev@gmail.com>
2023-10-24 17:56:58 -0500

    Create initial SurveyNew component

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// - Shows SurveyForm and SurveyFormReview.
+// - Toggles between SurveyForm and SurveyFormReview.
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SurveyNew component!</h1>
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
