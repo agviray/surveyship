793206143ff398dc3a7c2efe816b508f6aec012c
Allan Viray <allang.v.dev@gmail.com>
2023-10-24 20:42:09 -0500

    Add additional Fields in SurveyForm

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,14 +2,14 @@ import React from 'react';
 // - SurveyField contains logic to render a single
 //   label and text input.
 
-const SurveyField = ({ input }) => {
+const SurveyField = ({ input, label }) => {
   // - Several properties are automatically available on props, due
   //   to implemention of redux-form. (ie: the input prop)
   // console.log(props);
 
   return (
     <div>
-      <h2>SurveyField</h2>
+      <label>{label}</label>
       <input {...input} onBlur={input.onBlur} onChange={input.onChange} />
     </div>
   );
