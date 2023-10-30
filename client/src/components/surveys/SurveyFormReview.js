74a897fb2909b11a9fc918778c53590797cce81d
Allan Viray <allang.v.dev@gmail.com>
2023-10-30 15:24:08 -0500

    Display user inputs in SurveyFormReview field.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,23 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { formFields } from './formFields';
 
 // - Shows user their form inputs, for their review.
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ label, name }, index) => {
+    return (
+      <div key={index}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Confirm your survey entries!</h5>
-
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
