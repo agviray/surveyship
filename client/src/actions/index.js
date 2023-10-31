e8e0cb249e9b0ce07cb7e4ef2aff9eb1cacd089b
Allan Viray <allang.v.dev@gmail.com>
2023-10-30 20:58:33 -0500

    Update submitSurvey action creator to send form inputs to be used in survey sent to recipients

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,8 +13,9 @@ export const handleToken = (token) => async (dispatch) => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = (values) => {
-  return {
-    type: 'submit_survey',
-  };
+// - Submit survey to be sent out to recipients.
+export const submitSurvey = (values) => async (dispatch) => {
+  const res = await axios.post('/api/surveys', values);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
