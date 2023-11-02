4c227ad4d0bc178494e54399cb976d463d322edd
Allan Viray <allang.v.dev@gmail.com>
2023-11-02 17:49:09 -0500

    Display the survey body correctly in SurveyList

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -15,7 +15,7 @@ class SurveyList extends Component {
         <div className="card darken-1" key={survey._id}>
           <div className="card-content">
             <span className="card-title">{survey.title}</span>
-            <p>survey.body</p>
+            <p>{survey.body}</p>
             <p className="right">
               Sent On: {new Date(survey.dateSent).toLocaleDateString()}
             </p>
