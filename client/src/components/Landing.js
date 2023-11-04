3a1ffc3418fd945d1c0acfa57b2627bcd51eae7b
Allan Viray <allang.v.dev@gmail.com>
2023-11-04 14:50:41 -0500

    Add background video

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+import oceanVideo from '../assets/calm-ocean-aerial.mp4';
+
+const StyledWrapper = styled.div`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const StyledVideoWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+const StyledContent = styled.div`
+  position: relative;
+  text-align: center;
+  color: white;
+`;
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>SurveyShip</h1>
-      Collect feedback from users.
-    </div>
+    <StyledWrapper>
+      <StyledVideoWrapper>
+        <video playsInline autoPlay muted loop>
+          <source src={oceanVideo} type="video/mp4"></source>
+          Your browser does not support the video tag
+        </video>
+      </StyledVideoWrapper>
+      <StyledContent>
+        <h1>SurveyShip</h1>
+        Collect feedback from users.
+      </StyledContent>
+    </StyledWrapper>
   );
 };
 
