351880827516d4ca2988c6df81d022f9d5122df8
Allan Viray <allang.v.dev@gmail.com>
2023-11-04 17:22:15 -0500

    Create initial GlobalStyles

---
+++ client/src/components/styles/GlobalStyles.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {
+    min-width: 300px;
+    font-size: 16px;
+    font-family: Arial, Helvetica, sans-serif;
+    
+  }
+
+  /* header {
+  
+  } */
+
+  h1, h2, h3, h4 {
+    letter-spacing: 2px;
+   }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    text-indent: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  p {
+    letter-spacing: 0.025em;
+    line-height: 1.5;
+  }
+`;
+
+export default GlobalStyles;
